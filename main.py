cad9c7cf4692e0e05374b025e409aa8d212cd3ad
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:28:12 +0200

    první commit pokračování

--- main.py
+++ main.py
@@ -1,2 +1,58 @@
+from operator import truediv
+
 print("Hello world")
-print("Ahoj")
+
+a = "ahoj"
+b = a
+
+print(b) #komentář
+#komentář
+
+#datové typy v pythonu
+
+samopopisne_nazvy_promenych = 'ahoj'
+print(samopopisne_nazvy_promenych)
+print(type(samopopisne_nazvy_promenych))
+
+cislo = 9
+print(cislo)
+print(type(cislo))
+
+desetinne_cislo = 5.5
+print(desetinne_cislo)
+print(type(desetinne_cislo))
+
+vysledek = 5 / 7
+print(vysledek)
+print(type(vysledek))
+
+#cisla
+
+a = 5
+b = 7
+c = a + b
+d = a - b
+e = a * b + a / b
+f = 7 / 5
+g = a ** 2
+h = a ** (1/2)
+i = a % b
+
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
+x = 5.7
+print(round(x))
+print(int(x))
+
+y = 5 + 5 * 2
+print(y)
+
+logicka = True
+l2 = False
+llll = logicka and l2 or logicka
